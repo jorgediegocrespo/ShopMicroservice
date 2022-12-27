fabe0cdeee6540a567c7f359eecf5b8595999028
Jorge DevsDNA <jorge@devsdna.com>
2022-12-27 16:43:19 +0100

    Created api gateway. Currently is working without docker

---
+++ Shop.Api.Gateway/Program.cs
@@ -0,0 +1,77 @@
+﻿using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+using Shop.Api.Gateway.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var routes = "Routes";
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
+
+builder.Services.AddOcelot(builder.Configuration).AddPolly();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+
+}).UseOcelot().Wait();
+
+app.MapControllers();
+
+app.Run();
+
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Configuration.AddJsonFile("ocelot.books.json", optional: false, reloadOnChange: true);
+//builder.Services.AddOcelot();
+
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+//app.UseOcelot().Wait();
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+//else
+//{
+//    app.UseHttpsRedirection();
+//}
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
